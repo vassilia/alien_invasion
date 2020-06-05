1178aa4baaddcd7edc11eb58b092f0ced00080c1
vassiliacostarides <vcostarides@biomed.ntua.gr>
2020-06-05 20:17:34 +0300

    first version default color

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    #overall class to manage game assets and behavior
+
+    def __init__(self):
+        #initialize game
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        #start the main loop
+        while True:
+            #watch for keybord and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                   sys.exit()
+            #make recent screen visible
+            pygame.display.flip()
+if __name__=='__main__':
+   #make a game instance and run the game
+   ai = AlienInvasion()
+   ai.run_game()
