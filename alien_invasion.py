64cc86f2a0224b85e610aa7b8d40a1b9fe28616c
vassiliacostarides <vcostarides@biomed.ntua.gr>
2020-06-06 00:18:16 +0300

    bullets are firing

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         #background color
         self.bg_color = (230,230,230)
 
@@ -26,9 +28,11 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
-            # Redraw the screen during each pass through the loop.
 
+            print(len(self.bullets))
+            # Redraw the screen during each pass through the loop.
             #make the most recent screen visible
             pygame.display.flip()
     def _check_events(self):
@@ -49,6 +53,8 @@ class AlienInvasion:
               self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -57,10 +63,27 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
              self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        #create new bullet and add it to bullets group
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        #"""Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        # get rid of bullets that have reached top of the screen
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         #Update images on the screen, and flip to the new screen
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
 if __name__=="__main__":
    #maje a game instance and run the game
